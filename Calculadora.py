d0fb0e5c0d8eee0dda94639b837de19891754b08
Vinicius Muniz <100857772+VMunizO@users.noreply.github.com>
2023-03-27 12:31:12 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,5 @@
+import math
+
 # Função para adição
 def add(x, y):
    return x + y
@@ -20,12 +22,15 @@ print("1.Adicionar")
 print("2.Subtrair")
 print("3.Multiplicar")
 print("4.Dividir")
+print("5.Potência")
+print("6.Raiz Quadrada")
 
 # Entrada de opção do usuário
-choice = input("Digite sua opção (1/2/3/4): ")
+choice = input("Digite sua opção (1/2/3/4/5/6): ")
 
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
+if choice in ('1', '2', '3', '4'):
+   num1 = float(input("Digite o primeiro número: "))
+   num2 = float(input("Digite o segundo número: "))
 
 if choice == '1':
    print(num1,"+",num2,"=", add(num1,num2))
@@ -39,9 +44,17 @@ elif choice == '3':
 elif choice == '4':
    print(num1,"/",num2,"=", divide(num1,num2))
 
+elif choice == '5':
+   num1 = float(input("Digite a base: "))
+   num2 = float(input("Digite o expoente: "))
+   print(num1,"^",num2,"=", pow(num1, num2))
+
+elif choice == '6':
+   num = float(input("Digite um número: "))
+   print("A raiz quadrada de", num, "é", math.sqrt(num))
+
 else:
    print("Opção inválida")
-
-#Nesse exemplo, cada operação matemática é realizada em sua própria função, 
-#e a opção do usuário é lida como uma entrada e usada para decidir qual função executar. 
-#Observe que o float() é usado para converter a entrada do usuário em números de ponto flutuante, para que a divisão seja feita corretamente.
+#além das operações aritméticas básicas, as funções pow() e math.sqrt() são usadas para calcular potência e raiz quadrada, respectivamente. 
+#O programa verifica a opção do usuário e lê a entrada apropriada com base na operação selecionada. 
+#Observe que math.sqrt() é uma função da biblioteca padrão de Python e precisa ser importada antes de ser usada.
